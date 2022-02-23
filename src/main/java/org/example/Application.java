68844708e6d137dee620b851193d1c52c241f654
kriti <kriti.dhingra@credable.in>
2022-02-23 12:03:58 +0530

    Added controller Customers.java, also running on browsers and Postman

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,29 +1,33 @@
-package org.example.app;
+package org.example;
 
 import org.example.model.Customer;
 import org.example.service.CustomerService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 
+@SpringBootApplication
 public class Application{
 
     public static void main(String[] args) {
+        SpringApplication.run(Application.class);
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        CustomerService service = context.getBean("customerService", CustomerService.class);
-
-
-        List<Customer>customers = service.getAllCustomers();
+//        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//
+//        CustomerService service = context.getBean("customerService", CustomerService.class);
+//
+//
+//        List<Customer>customers = service.getAllCustomers();
 
        // CustomerRepositoryStub customerRepositoryStub = new CustomerRepositoryStub();
         //CustomerRespositoryDB dbrepository = new CustomerRespositoryDB();
 
         //DefaultCustomerService service = new DefaultCustomerService(dbrepository);
 
-        customers.forEach(System.out::println);
+//        customers.forEach(System.out::println);
     }
 
 
--- src/main/java/org/example/service/DefaultCustomerService.java
+++ src/main/java/org/example/service/DefaultCustomerService.java
@@ -2,14 +2,16 @@ package org.example.service;
 
 import org.example.model.Customer;
 import org.example.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@Service("cutomerService")
 public class DefaultCustomerService implements CustomerService {
 
-
+    @Autowired
     private CustomerRepository repository;
 
     //setter injection
